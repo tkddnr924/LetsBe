907a08741a92caedc579d81530887321b624fce4
Sangwook-Ahn <tkddnr924@naver.com>
2017-05-30 05:20:25 +0900

    Home Page 뼈대 생성

--- LetsGo/LetsGo/urls.py
+++ LetsGo/LetsGo/urls.py
@@ -16,6 +16,12 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
+from LetsGo.views import HomeView
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+
+    # Example : /
+    # Homepage URL
+    url(r'^$', HomeView.as_view(), name='home'),
 ]
